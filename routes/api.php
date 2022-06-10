06382d52ba70648d8138b8ff28deb0d0f35fc1dc
ishani.r <Ishani.r@elaunchinfotech.in>
2022-06-10 20:30:49 +0530

    create api controller

--- routes/api.php
+++ routes/api.php
@@ -17,3 +17,6 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+// Register
+Route::post('registration', [RegisterController::class, 'create']);
