ef4d47a89dfcde2ee4a4c3467e56cb5fcca99e57
ishani.r <Ishani.r@elaunchinfotech.in>
2022-06-11 04:16:15 +0530

    change column name

--- app/Repositories/LoginRepository.php
+++ app/Repositories/LoginRepository.php
@@ -14,7 +14,7 @@ class LoginRepository implements LoginContract
       DB::beginTransaction();
       try {
          $otp = mt_rand(1000, 9999);
-         $user_data = FarmerUser::where('raw_mobile', $data['mobile'])->first();
+         $user_data = FarmerUser::where('raw_mobile', $data['raw_mobile'])->first();
          if ($user_data) {
             $user_data->raw_otp = $otp;
             $user_data->save();
@@ -22,7 +22,7 @@ class LoginRepository implements LoginContract
             return $user_data;
          } else {
             $user = new FarmerUser();
-            $user->raw_mobile = $data['mobile'];
+            $user->raw_mobile = $data['raw_mobile'];
             $user->raw_otp = $otp;
             $user->save();
             DB::commit();
